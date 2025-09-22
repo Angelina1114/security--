8e45f4729c3b6f168d6397e93462b5fccb4640cf
Angelina <angelinalu1114@gmail.com>
2025-09-22 12:14:32 +0800

    first commit

---
+++ 1.py
@@ -0,0 +1,6 @@
+print("Enter the number of rows for the pyramid:")
+n = int(input())
+for i in range(1, n+1):
+    spaces = ' ' * (n - i)
+    stars = '*' * (2 * i - 1)
+    print(spaces + stars + spaces)
